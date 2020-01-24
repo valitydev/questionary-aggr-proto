f1611dc253fea94b22cd21ebe2840a4d68adf2e9
Anatolii Karlov <a.karlov@rbkmoney.com>
2020-01-24 14:58:07 +0300

    BJ-763: update protocol (#9)

--- proto/questionary_proxy_aggr.thrift
+++ proto/questionary_proxy_aggr.thrift
@@ -4,12 +4,18 @@ namespace erlang questionary_proxy_aggr
 include "kontur_focus_api.thrift"
 include "dadata_api.thrift"
 
-exception KonturFocusRequestException {
+exception KonturFocusInvalidRequest {
     1: required string error_msg
 }
 
-exception DaDataRequestException {
+exception DaDataInvalidRequest {
     1: required string error_msg
+}
+
+exception KonturFocusNotFound {
+}
+
+exception DaDataNotFound {
 }
 
 service QuestionaryAggrProxyHandler {
@@ -17,13 +23,11 @@ service QuestionaryAggrProxyHandler {
     kontur_focus_api.KonturFocusResponse RequestKonturFocus(
                 1: kontur_focus_api.KonturFocusRequest request,
                 2: kontur_focus_api.KonturFocusEndPoint endpoint
-            ) throws (1: KonturFocusRequestException ex)
+            ) throws (1: KonturFocusInvalidRequest ex1, 2: KonturFocusNotFound ex2)
 
     dadata_api.DaDataResponse RequestDaData(
                 1: dadata_api.DaDataRequest request,
                 2: dadata_api.DaDataEndpoint endpoint
-            ) throws (1: DaDataRequestException ex)
+            ) throws (1: DaDataInvalidRequest ex1, 2: DaDataNotFound ex2)
 
 }
-
-
